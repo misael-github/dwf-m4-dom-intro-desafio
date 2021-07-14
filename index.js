d8775dc3d7a7535be8be12f0ba3ab87f68e52a6e
Misael <dmisael2000@gmail.com>
2021-07-14 16:55:01 -0300

    DOM

--- index.js
+++ index.js
@@ -25,6 +25,21 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listaEls = document.querySelectorAll(".lista li");
+
+//  variable   | objeto o elemento
+  for (let el of listaEls) {
+    el.remove();
+  }
+
+  for (let item of cosasQueAprendimos) {
+    const newLiEl = document.createElement("li"); // Creamos las etiquetas li
+    newLiEl.textContent = item.tema; // Le ponemos de texto lo que tengo en la prop. tema de cada objeto
+    newLiEl.classList.add(item.class || "item"); // Le añadimos la clase que tenemos en los objetos o item
+    const lista = document.querySelector(".lista"); // Seleccionamos la lista
+    lista.appendChild(newLiEl); // Añadimos al elemento que le quiero agregar y (elemento que agrego)
+  }
+}
 
 main();
